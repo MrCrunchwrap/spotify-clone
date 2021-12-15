ce87c0919bd58f687638318582627dd9e0709d72
Michael McDonald <mcdonald.michael.mn@gmail.com>
2021-12-15 14:43:06 -0600

    fixing TS issues for build

--- components/player.tsx
+++ components/player.tsx
@@ -21,10 +21,9 @@ import {
   MdOutlineRepeat,
 } from "react-icons/md";
 import { useStoreActions } from "easy-peasy";
-import { Song } from "@prisma/client";
 import { formatTime } from "../lib/formatters";
 
-const Player = ({ songs, activeSong }: { songs: Song[]; activeSong: Song }) => {
+const Player = ({ songs, activeSong }) => {
   const [playing, setPlaying] = useState(true);
   const [index, setIndex] = useState(
     songs.findIndex((s) => s.id === activeSong.id)
@@ -36,10 +35,10 @@ const Player = ({ songs, activeSong }: { songs: Song[]; activeSong: Song }) => {
   const [duration, setDuration] = useState(0.0);
   const soundRef = useRef(null);
   const repeatRef = useRef(repeat);
-  const setActiveSong = useStoreActions((state: any) => state.setActiveSong);
+  const setActiveSong = useStoreActions((state: any) => state.changeActiveSong);
 
   useEffect(() => {
-    let timerId: number;
+    let timerId;
 
     if (playing && !isSeeking) {
       const f = () => {
@@ -52,7 +51,7 @@ const Player = ({ songs, activeSong }: { songs: Song[]; activeSong: Song }) => {
     }
 
     cancelAnimationFrame(timerId);
-  }, [isSeeking, playing]);
+  }, [playing, isSeeking]);
 
   useEffect(() => {
     setActiveSong(songs[index]);
@@ -83,7 +82,6 @@ const Player = ({ songs, activeSong }: { songs: Song[]; activeSong: Song }) => {
   const nextSong = () => {
     setIndex((state) => {
       if (shuffle) {
-        // shuffle logic
         const next = Math.floor(Math.random() * songs.length);
 
         if (next === state) {
@@ -91,14 +89,13 @@ const Player = ({ songs, activeSong }: { songs: Song[]; activeSong: Song }) => {
         }
         return next;
       }
-      // serial playing
+
       return state === songs.length - 1 ? 0 : state + 1;
     });
   };
 
   const onEnd = () => {
     if (repeatRef.current) {
-      // repeat song
       setSeek(0);
       soundRef.current.seek(0);
     } else {
@@ -198,8 +195,8 @@ const Player = ({ songs, activeSong }: { songs: Song[]; activeSong: Song }) => {
               aria-label={["min", "max"]}
               step={0.1}
               min={0}
-              max={duration ? duration.toFixed(2) : 0}
               id="player-range"
+              max={duration ? (duration.toFixed(2) as unknown as number) : 0}
               onChange={onSeek}
               value={[seek]}
               onChangeStart={() => setIsSeeking(true)}
